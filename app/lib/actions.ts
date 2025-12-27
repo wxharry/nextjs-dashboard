ce5137bf16f0a3d36dd073635aba7d722af2b7d4
Harry <xiaohanwu12@gmail.com>
2025-12-27 20:51:44 +1300

    fix: updateInvoice function signature to include prevState parameter

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -69,7 +69,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
   redirect('/dashboard/invoices');
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
+export async function updateInvoice(id: string, prevState: State,formData: FormData) {
    const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
